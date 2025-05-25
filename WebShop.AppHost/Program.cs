d025e27f57bb9fb888b747d2f520d08a4e8a7f1e
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2025-05-25 17:31:49 +0100

    add rabbitmq and

--- WebShop.AppHost/Program.cs
+++ WebShop.AppHost/Program.cs
@@ -1,14 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
-
+var rabbitMq = builder.AddRabbitMQ("rabbitmq"); 
+   
 var apiService = builder.AddProject<Projects.WebShop_ApiService>("apiservice");
+var orderingAPI = builder.AddProject<Projects.WebShop_OrderingAPI>("orderingapi");
 
 builder.AddProject<Projects.WebShop_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WithReference(apiService);
-
-builder.AddProject<Projects.WebShop_OrderingAPI>("webshop-orderingapi");
 
 builder.Build().Run();
