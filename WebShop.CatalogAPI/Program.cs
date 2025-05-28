2fb79c91d86caa4ab0c15e6c6d53cfa96be70dda
Shakirudeen Lasisi <shakirudeen.lasisi@nottingham.ac.uk>
2025-05-28 21:03:27 +0100

    cleanup catalog api

--- WebShop.CatalogAPI/Program.cs
+++ WebShop.CatalogAPI/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using WebShop.CatalogAPI.Data;
 using WebShop.CatalogAPI;
 
@@ -21,31 +19,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
 app.MapDefaultEndpoints();
 
 app.MapItemEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
